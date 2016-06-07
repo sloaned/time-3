0d4a50755d9aa74ba3409d233a3835b36699e730
Daniel Sloane <dsloane@catalystdevworks.com>
2016-06-07 12:49:34 -0700

    beefed up user entity

---
+++ src/main/java/com/catalystdevworks/mtidwell/timeclock/entity/Role.java
@@ -0,0 +1,9 @@
+package com.catalystdevworks.mtidwell.timeclock.entity;
+
+/**
+ * Created by dsloane on 6/7/2016.
+ */
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/catalystdevworks/mtidwell/timeclock/entity/User.java
+++ src/main/java/com/catalystdevworks/mtidwell/timeclock/entity/User.java
@@ -10,6 +10,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
@@ -23,15 +26,36 @@ public class User implements RowMapper<User>{
 	public static final RowMapper<User> ROW_MAPPER = new User();
 	
 	public static final String COLUMN_ID = "id";
-	public static final String COLUMN_FULL_NAME = "fullName";
 	public static final String COLUMN_BIRTHDAY = "birthday";
 	public static final String COLUMN_CREATED_ON = "createdOn";
+	public static final String COLUMN_FIRST_NAME = "firstName";
+	public static final String COLUMN_LAST_NAME = "lastName";
+	public static final String COLUMN_USERNAME = "username";
+	public static final String COLUMN_PASSWORD = "password";
+	public static final String COLUMN_ACTIVE = "active";
+	public static final String COLUMN_ROLE = "role";
 	
 	private UUID id;
-	
-	@Size(min=3, max=30)
-	private String fullName;
-	
+
+	@NotNull(message="First name is required")
+	private String firstName;
+
+	@NotNull(message="Last name is required")
+	private String lastName;
+
+	@NotNull(message="Username is required")
+	private String username;
+
+	@NotNull(message="Password is required")
+	private String password;
+
+	@NotNull(message="User must be active or inactive")
+	private Boolean active;
+
+	@NotNull
+	@Enumerated(value = EnumType.STRING)
+	private Role role;
+
 	@JsonFormat(shape=JsonFormat.Shape.STRING)
 	private ZonedDateTime createdOn;
 
@@ -41,9 +65,13 @@ public class User implements RowMapper<User>{
 	@Override
 	public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 		User user = new User();
-		
 		user.setId(UUID.fromString(rs.getString(COLUMN_ID)));
-		user.setFullName(rs.getString(COLUMN_FULL_NAME));
+		user.setFirstName(rs.getString(COLUMN_FIRST_NAME));
+		user.setLastName(rs.getString(COLUMN_LAST_NAME));
+		user.setUsername(rs.getString(COLUMN_USERNAME));
+		user.setPassword(rs.getString(COLUMN_PASSWORD));
+		user.setActive(rs.getBoolean(COLUMN_ACTIVE));
+		user.setRole(Role.valueOf(rs.getString(COLUMN_ROLE)));
 		user.setBirthday(DateTimeFormatter.ISO_DATE.parse(rs.getString(COLUMN_BIRTHDAY), LocalDate::from));
 		user.setCreatedOn(DateTimeFormatter.ISO_DATE_TIME.parse(rs.getString(COLUMN_CREATED_ON), ZonedDateTime::from));
 		
@@ -54,7 +82,12 @@ public class User implements RowMapper<User>{
 		Map<String, Object> sqlData = new HashMap<>();
 		
 		sqlData.put(COLUMN_ID, id.toString());
-		sqlData.put(COLUMN_FULL_NAME, fullName);
+		sqlData.put(COLUMN_FIRST_NAME, firstName);
+		sqlData.put(COLUMN_LAST_NAME, lastName);
+		sqlData.put(COLUMN_USERNAME, username);
+		sqlData.put(COLUMN_PASSWORD, password);
+		sqlData.put(COLUMN_ACTIVE, active);
+		sqlData.put(COLUMN_ROLE, role.toString());
 		sqlData.put(COLUMN_BIRTHDAY, DateTimeFormatter.ISO_DATE.format(birthday));
 		sqlData.put(COLUMN_CREATED_ON, DateTimeFormatter.ISO_DATE_TIME.format(createdOn.withZoneSameInstant(ZoneOffset.UTC)));
 		
@@ -72,12 +105,19 @@ public class User implements RowMapper<User>{
 	public void setId(UUID id) {
 		this.id = id;
 	}
-	public String getFullName() {
-		return fullName;
-	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+	public String getFirstName() { return firstName; }
+	public void setFirstName(String firstName) { this.firstName = firstName; }
+	public String getLastName() { return lastName; }
+	public void setLastName(String lastName) { this.lastName = lastName; }
+	public String getUsername() { return username; }
+	public void setUsername(String username) { this.username = username; }
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+	public Boolean isActive() { return active; }
+	public void setActive(Boolean active) { this.active = active; }
+	public Role getRole() { return role; }
+	public void setRole(Role role) { this.role = role; }
+
 	public ZonedDateTime getCreatedOn() {
 		return createdOn;
 	}
