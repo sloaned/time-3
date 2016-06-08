2ad6f4052e0f96201537f3edae02bda6b4120585
Daniel Sloane <dsloane@catalystdevworks.com>
2016-06-08 13:01:11 -0700

    added form validation on registration page

--- src/main/javascript/register/RegisterController.js
+++ src/main/javascript/register/RegisterController.js
@@ -10,8 +10,23 @@ module.exports = [
             $scope.lastName = "";
             $scope.username = "";
             $scope.password = "";
+            $scope.confirmPassword = "";
             $scope.email = "";
             $scope.birthday = "";
+            $scope.firstNameError = false;
+            $scope.lastNameError = false;
+            $scope.usernameError = false;
+            $scope.passwordError = false;
+            $scope.passwordMatchError = false;
+            $scope.emailError = false;
+
+            $scope.user = {};
+
+            $scope.register = function() {
+
+                validateFields();
+            }
+
 
 
             $scope.clearFields = function() {
@@ -20,8 +35,31 @@ module.exports = [
                 $scope.lastName = "";
                 $scope.username = "";
                 $scope.password = "";
+                $scope.confirmPassword = "";
                 $scope.email = "";
                 $scope.birthday = "";
+                $scope.firstNameError = false;
+                $scope.lastNameError = false;
+                $scope.usernameError = false;
+                $scope.passwordError = false;
+                $scope.passwordMatchError = false;
+                $scope.emailError = false;
+            };
+
+
+            var validateFields = function() {
+                if ($scope.firstName === null || $scope.firstName.length == 0) { $scope.firstNameError = true; }
+                else { $scope.firstNameError = false; }
+                if ($scope.lastName === null || $scope.lastName.length == 0) { $scope.lastNameError = true; }
+                else { $scope.lastNameError = false; }
+                if ($scope.username === null || $scope.username.length == 0) { $scope.usernameError = true; }
+                else { $scope.usernameError = false; }
+                if ($scope.password === null || $scope.password.length == 0) { $scope.passwordError = true; }
+                else { $scope.passwordError = false; }
+                if ($scope.confirmPassword === null || $scope.confirmPassword != $scope.password) { $scope.passwordMatchError = true; }
+                else { $scope.passwordMatchError = false; }
+                if ($scope.email === null || $scope.email.length == 0) { $scope.emailError = true; }
+                else { $scope.emailError = false; }
             };
 
 	    }
